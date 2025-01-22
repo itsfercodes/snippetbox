9933008f433d68f522dbad2a38fc8df9060a50af
Fernando Lopez <itsfercodes@gmail.com>
2025-01-21 22:49:41 -0600

    Add POST route

--- main.go
+++ main.go
@@ -26,14 +26,19 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Form for create a snippet"))
 }
 
+func postSnippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Saving new snippet..."))
+}
+
 func main() {
 	// A ServeMux is a router
 	mux := http.NewServeMux()
 
 	// All unknown routes will lead to the "/" route if "{$}" is not added
-	mux.HandleFunc("/{$}", home) // Restrict this route to exact matches on / only.
-	mux.HandleFunc("/snippet/view/{id}", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("GET /{$}", home) // Restrict this route to exact matches on / only.
+	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
+	mux.HandleFunc("GET /snippet/create", snippetCreate)
+	mux.HandleFunc("POST /snippet/create", postSnippetCreate)
 
 	log.Print("starting server on :4000")
 
